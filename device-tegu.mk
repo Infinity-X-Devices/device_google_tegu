b0de309552bc210109f9643ba8b3009021675456
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:29 +0530

    tegu: The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- device-tegu.mk
+++ device-tegu.mk
@@ -32,6 +32,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
 
+# Google Camera
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/sysconfig/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0-service \
